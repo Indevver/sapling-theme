a0de3fbfb8546127d8a748d7a7ae9268707fef25
Robert Parker <diaboloshogunate@gmail.com>
2019-05-16 12:21:24 -0700

    limit headiing sizes

--- src/ACF/Fields/Heading.php
+++ src/ACF/Fields/Heading.php
@@ -13,10 +13,6 @@ class Heading extends FieldsBuilder
             ->addSelect('Size', ['choices' => [
                 'h1' => 'Heading 1',
                 'h2' => 'Heading 2',
-                'h3' => 'Heading 3',
-                'h4' => 'Heading 4',
-                'h5' => 'Heading 5',
-                'h6' => 'Heading 6',
             ]])->setWidth('50')
             ->addText('Heading')->setWidth('50')
         ;
