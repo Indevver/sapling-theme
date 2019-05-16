e85e9a48a4dfe5d6a176c4643d92c6ced00dd4aa
Robert Parker <diaboloshogunate@gmail.com>
2019-05-16 12:22:09 -0700

    change images block layout

--- src/ACF/Fields/Image.php
+++ src/ACF/Fields/Image.php
@@ -10,15 +10,15 @@ class Image extends FieldsBuilder
     {
         parent::__construct('image', ['label' => 'Image']);
         $this->addTab('Content')
-            ->addImage('Image')->setWidth('50')
-            ->addTrueFalse('Full Width')->setWidth('50')->setDefaultValue(true)
-            ->addField('Text Alignment', 'button_group', [
+            ->addField('Image Alignment', 'button_group', [
                 'choices' => [
-                    "left" => "<span class=\"dashicons dashicons-editor-alignleft\"></span>",
-                    "center" => "<span class=\"dashicons dashicons-editor-aligncenter\"></span>",
-                    "right" => "<span class=\"dashicons dashicons-editor-alignright\"></span>",
+                    "left" => "<span class=\"dashicons dashicons-align-left\"></span>",
+                    "center" => "<span class=\"dashicons dashicons-align-center\"></span>",
+                    "right" => "<span class=\"dashicons dashicons-align-right\"></span>",
                 ]
             ])
+            ->addImage('Image')->setWidth('50')
+            ->addTrueFalse('Full Width')->setWidth('50')->setDefaultValue(true)
         ;
     }
 }
