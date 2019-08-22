e7b9d4c140e4e4d7acae5df695a5ec54cb93461f
Robert Parker <diaboloshogunate@gmail.com>
2019-08-22 11:25:37 -0700

    move customizer and login to plugin

--- resources/functions.php
+++ resources/functions.php
@@ -6,8 +6,6 @@ require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/../includes/theme-requirements.php';
 require_once __DIR__.'/../includes/timber.php';
 require_once __DIR__.'/../includes/builder.php';
-require_once __DIR__.'/../includes/customizr.php';
-require_once __DIR__.'/../includes/login-styles.php';
 
 $theme = new Theme();
 $pageBuilder = new PageBuilder();
