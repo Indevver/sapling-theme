e02120691c20660866d8f31139be39531b9985b7
Robert Parker <diaboloshogunate@gmail.com>
2019-11-06 13:26:50 -0800

    load vendor first

--- resources/functions.php
+++ resources/functions.php
@@ -3,10 +3,11 @@ use Sapling\Plugin\ACF\PageBuilder;
 use Sapling\Theme;
 use Symfony\Component\Dotenv\Dotenv;
 
+require_once __DIR__.'/../vendor/autoload.php';
+
 $dotenv = new Dotenv();
 $dotenv->loadEnv(__DIR__.'/../.env');
 
-require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/../includes/theme-requirements.php';
 require_once __DIR__.'/../includes/timber.php';
 require_once __DIR__.'/../includes/builder.php';
