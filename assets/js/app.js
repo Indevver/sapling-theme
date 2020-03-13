fe496494aa778bcc468348b3bb778de8b8062a1e
Robert Parker <diaboloshogunate@gmail.com>
2020-03-13 15:26:30 -0700

    remove/disable slick/lozad/fancybox by default

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,20 +4,19 @@ import Vue from 'vue'
 import '@fancyapps/fancybox';
 import 'slick-carousel';
 
-new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!',
-    },
-});
+// new Vue({
+//     el: '#app',
+//     data: {
+//         message: 'Hello Vue!',
+//     },
+// });
+//
+// jQuery('.fancybox').fancybox({
+//
+// });
+//
+// jQuery('.slick').slick({
+//
+// });
 
-const observer = lozad();
-observer.observe();
-
-jQuery('.fancybox').fancybox({
-
-});
-
-jQuery('.slick').slick({
-
-});
+console.log("loaded");
